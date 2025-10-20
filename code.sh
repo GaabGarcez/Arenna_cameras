c008df21f2131abebdbf03228b15f954bd4f6fdf
GaabGarcez <antoniogabrielgarcezbarbosa@gmail.com>
2025-10-20 20:34:11 -0300

    feat: adiciona o gitignore e o code.sh

---
+++ code.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y \
+  python3-opencv python3-pip python3-venv \
+  ffmpeg \
+  gstreamer1.0-tools gstreamer1.0-libav \
+  gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
+  libatlas3-base
